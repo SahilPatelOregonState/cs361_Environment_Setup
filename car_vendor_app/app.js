f8030b47150e79a7fbeb88661af626438ed23249
SahilPatelOregonState <patelsah@oregonstate.edu>
2023-02-01 18:26:36 -0500

    Should be for version 1.0.0 links to homepage car_search, car_wishlist first write to Output.json file for microservice

--- car_vendor_app/app.js
+++ car_vendor_app/app.js
@@ -1,22 +1,91 @@
 const express = require('express');
 const app = express();
-PORT = 3000;
+const PORT = 3000;
+const fs = require('fs/promises');
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+//app.use(pipelining)
 
 //Loads the handlebars module
-const handlebars = require('express-handlebars');
+var exphbs = require('express-handlebars');
+const { pipeline } = require('stream');
 //Sets our app to use the handlebars engine
-app.set('view engine', 'hbs');
+
 //Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
+
+
+app.engine('.hbs', exphbs.engine({
 layoutsDir: __dirname + '/views/layouts',
 defaultLayout: 'index',
-extname:'hbs',
-partialsDir: __dirname + '/views/partials'
+extname:'.hbs',
+partialsDir: __dirname + '/views/partials',
+
+helpers:{
+    CarSearchWriteFile : async (value, options) =>{
+         fs.writeFile('Output.txt',value,(err)=>{
+            
+            if ((err) || (value ==='')) throw err;
+
+         } )
+    },
+}
 
 }));
+app.set('view engine', '.hbs');
 
-app.get('/', (req, res) => {
+app.listen(PORT,()=>{console.log(`Listening on port ${PORT}`)});
+
+app.get('/', async(req, res) => {
     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-    res.render('main', {layout : 'index'});
-    });
+    res.render('main', {layout : 'index', title:'Home Page' });
+
+
+});
+
+
+app.get('/car_search', async(req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('car_search', {layout : 'index', title: 'Car Search', data_retrieved:true});
+
+   
+})
+
+app.post('/car_search', async(req, res) => {
+    //Serves the body of the page aka "car_wishlist.handlebars" to the container //aka "index.handlebars"
+res.render('car_search', {layout : 'index', title:'Car Search',data_retrieved:true});
+var formData = req.body;
+var formDataJson = JSON.stringify(formData);
+if(formData.cname === ""){
+    console.log('No submission')
+    res.send('<script>alert("you did not enter a car name"); window.location.href = "/car_search"; </script>');
+    
+}
+else{
+    
+    console.log(formDataJson)
+    fs.writeFile("microservice/Output.json",formDataJson)
+
+    
+    
+
+}
+
+/**async function pipelining(req,res,formDataJson, next){
+    try{
+        fs.writeFileSync("microservice/Output.json",formDataJson)
+    }
+    catch(err){console.log(error)}
+
+}*/
+
+
+
+});
+
+
+app.get('/car_wishlist', (req, res) => {
+        //Serves the body of the page aka "car_wishlist.handlebars" to the container //aka "index.handlebars"
+    res.render('car_wishlist', {layout : 'index', title:'Car Wishlist'});
+});
